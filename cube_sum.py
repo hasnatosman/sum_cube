591532262501e7f8775c7a7301f83dd1834b2542
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-12-08 23:21:11 +0600

    Add files via upload
    here is the output of the number.

---
+++ cube_sum.py
@@ -0,0 +1,10 @@
+def cube_sum(num):
+    sum = 0
+    for n in range(num + 1):
+        sum = sum + n ** 3
+    return sum
+
+
+user_num = int(input('Enter a number: '))
+result = cube_sum(user_num)
+print('Your sum of cubes are: ', result)
